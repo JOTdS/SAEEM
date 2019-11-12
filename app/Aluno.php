d1e01c233706fdfaac9e65ff869e6fd7bf2f6ea3
JOTdS <j.otavio.ts@gmail.com>
2019-11-12 17:41:15 -0300

    Mudando validações do controller para o model

--- app/Aluno.php
+++ app/Aluno.php
@@ -3,8 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Aluno extends Model
 {
-    //
+    protected $fillable = [
+        'filiacao'
+    ];
+
+    public static $rules = [
+        'filiacao' => 'required|max:200|min:5|string'
+    ];
+
+    public static $messages = [
+        'required' => 'O Campo :attribute é obrigatório',
+        'string' => 'O campo :atribute deve ser um texto',
+        'filiacao.max' => 'A filiação é muito grande (max 200 caracteres)'
+    ];
 }
--- app/Pessoa.php
+++ app/Pessoa.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pessoa extends Model
 {
-    //
+    protected $fillable = [
+        'nome',
+        'cpf',
+        'endereco',
+        'sexo'
+    ];
+
+    public static $rules = [        
+        'nome' => 'required|max:100|string',
+        'cpf' => 'required|unique:pessoas|max:11|min:11',
+        'telefone' => 'required',
+        'endereco' => 'max:255|string',
+        'descricao' => 'max:255|string',
+        'sexo' => 'required|max:1|string'
+    ];
+
+    public static $messages = [
+        'required' => 'O Campo :attribute é obrigatório',
+        'string' => 'O campo :atribute deve ser um texto',
+        'nome.max' => 'O nome é muito grande (max 100 caracteres)',
+        'sexo.max' => 'O sexo deve ser definido com a inicial do gênero',
+        'endereco.max' => 'O endereço é muito grande (max 255 caracteres)',
+        'cpf.max' => 'O cpf deve ser definido com 11 caracteres',
+        'cpf.unique' => 'O cpf já foi cadastrado'
+    ];
 }
