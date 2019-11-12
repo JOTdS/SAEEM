bd27aecb31e0fb36499dc6fd29c238bffc7613f0
JOTdS <j.otavio.ts@gmail.com>
2019-11-12 08:12:31 -0300

    Fazendo a ligação de users com pessoas

--- database/migrations/2019_11_05_194438_create_pessoas_table.php
+++ database/migrations/2019_11_05_194438_create_pessoas_table.php
@@ -21,7 +21,7 @@ class CreatePessoasTable extends Migration
             $table->text('endereco')->nullable();
             $table->text('descricao')->nullable();
             $table->char('sexo', 1);
-            $table->string('password');
+            $table->bigInteger('usuario_id')->nullable();
             $table->boolean('is_funcionario')->default(false);
             $table->boolean('is_aluno')->default(false);
             $table->timestamps();
